7b6173947d8b8e2c0308ab9678fe5775b467189e
Ignacio <128334221+Nachobass@users.noreply.github.com>
2025-03-26 19:15:28 -0300

    Add files via upload

---
+++ TP1-ARM/inputs/lsl.s
@@ -0,0 +1,16 @@
+movz X0, 0x10
+lsl X1, X0, 2
+HLT 0
+mov X0, 0x10
+lsl X1, X0, 60
+HLT 0
+mov X0, 0x10
+lsl X1, X0, 4
+lsl X2, X0, 8
+lsl X3, X0, 16
+lsl X4, X0, 32
+HLT 0
+mov X0, 0x1
+lsl X1, X0, 1
+lsl X2, X0, 31
+HLT 0
