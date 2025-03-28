8d3055be95015ead74874ac365c651b9b9512fb3
Ignacio <128334221+Nachobass@users.noreply.github.com>
2025-03-28 14:12:47 -0300

    more

--- TP1-ARM/inputs/br.s
+++ TP1-ARM/inputs/br.s
@@ -3,15 +3,15 @@
 
 _start:
     // Guardamos la dirección de retorno manualmente
-    ADR X30, return_point  // X30 = dirección de "return_point"
-    B funcion              // Salta a "funcion"
+    LDR X30, =return_point  // X30 = dirección de return_point
+    B funcion               // Salta a "funcion"
 
 return_point:
     // Esto se ejecuta cuando regresamos de la función
-    ADDS X1, X0, #1        // X1 = 1
+    ADDS X1, X0, #1         // X1 = 1
     HLT #0
 
 // Función que se salta
 funcion:
-    ADDS X2, X0, #2        // X2 = 2 (se ejecuta si B funcionó)
-    BR X30                 // Volver a la dirección guardada en X30
+    ADDS X2, X0, #2         // X2 = 2 (se ejecuta si B funcionó)
+    BR X30                  // Volver a la dirección guardada en X30
