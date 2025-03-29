431680ef4080b0b3e21ef3d6baafafe6217c3d42
Ignacio <128334221+Nachobass@users.noreply.github.com>
2025-03-29 07:50:29 -0300

    changes

--- TP1-ARM/inputs/add_mul.s
+++ TP1-ARM/inputs/add_mul.s
@@ -1,16 +1,13 @@
 .text
-// Test para ADD Immediate
 mov X1, 5
-add X2, X1, 3  // X2 = X1 + 3
+add X2, X1, 3
 
-// Test para ADD Extended
 mov X3, 4
 mov X4, 2
-add X5, X3, X4, LSL 1  // X5 = X3 + (X4 << 1) = 4 + (2 * 2) = 8     RARO ME DA 6
+add X5, X3, X4, LSL 1
 
-// Test para MUL
 mov X6, 3
 mov X7, 7
-mul X8, X6, X7  // X8 = X6 * X7 = 3 * 7 = 21        RARO ME DA 15
+mul X8, X6, X7
 
 HLT 0
