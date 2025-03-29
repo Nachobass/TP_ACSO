e5b57d5f031986a72aae7af5cd4902d21f60cd64
Ignacio <128334221+Nachobass@users.noreply.github.com>
2025-03-29 12:06:55 -0300

    changes

--- TP1-ARM/compare_everything.py
+++ TP1-ARM/compare_everything.py
@@ -5,10 +5,10 @@
 
 import subprocess
 
-file_names = ["add_mul.x", "add.x", "addis.x", "adds-subs.x", "adds.x", "ands.x", "b.x", "beq.x", "bge.x",
-              "bgt.x", "ble.x", "blt.x", "bne.x", "br.x", "cbnz.x", "cbz_cbnz.x", "cbz.x", "cmp_ex.x", 
-              "cmp.x", "eor.x", "lsl.x", "lsr_lsl.x", "lsr.x", "movz.x", "mul.x", "orr.x", 
-              "sturb.x", "subis.x"]
+file_names = ["add_mul.x", "add.x", "addis.x", "adds_add_subs.x", "adds-subs.x", "adds.x", "ands.x", "b.x", "beq.x", "bge.x",
+              "bgt.x", "ble.x", "blt.x", "bne.x", "br.x", "branch_if.x", "cbnz.x", "cbz_cbnz.x", "cbz.x", "cmp_ex.x", 
+              "cmp.x", "conditional_branches.x", "eor.x", "ldurh.x", "lsl.x", "lsr_lsl.x", "lsr.x", "memory_operations_and_shifts.x", 
+              "movz.x", "mul.x", "orr.x", "sturb.x", "sturh.x", "subis.x"]
 
 expected_message_1 = "✅ No differences found in dumpsim files."
 expected_message_2 = "✅ All simulations returned the same memory dump for the given cycles."
