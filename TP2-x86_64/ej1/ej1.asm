32294f386b47af9e07cb2bb175610abf97fcde71
Ignacio <128334221+Nachobass@users.noreply.github.com>
2025-04-17 08:30:16 -0300

    old

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -149,164 +149,108 @@ string_proc_list_add_node_asm:
 
 
 
+section .data
+    MAX_RESULT_LEN equ 1048576  ; 1 MB máximo
+
+section .text
 string_proc_list_concat_asm:
     ; rdi = list, rsi = type, rdx = hash
     push rbp
     mov rbp, rsp
-    sub rsp, 96            ; reservar espacio local
+    xor rax, rax
 
-    mov [rbp-72], rdi      ; list
-    movzx eax, sil         ; pasar type como 8 bits → eax
-    mov [rbp-88], rdx      ; hash
-    mov [rbp-76], al       ; guardar type en [rbp-76]
-
-    cmp qword [rbp-72], 0
+    test rdi, rdi            ; si list == NULL
     je .return_null
-    cmp qword [rbp-88], 0
-    jne .continue
-.return_null:
-    mov rax, 0
-    jmp .done
 
-.continue:
-    mov rax, [rbp-72]
-    mov rax, [rax]
-    mov [rbp-8], rax       ; aux1
-    mov rax, [rbp-72]
-    mov rax, [rax]
-    mov [rbp-16], rax      ; current
+    mov r8, rdi              ; guardo list en r8 antes de pisar rdi
+    mov r9b, sil             ; guardo type → r9b   (r9b:parte baja de r9), (sil:parte baja de rsi)
 
-.loop_check:
-    mov rax, [rbp-8]
-    mov rax, [rax]
-    mov [rbp-8], rax
+    ; rdx = hash
+    mov rdi, rdx             ; argumento para strlen(hash)
+    call strlen              ; rax = strlen(hash)
+    mov rcx, rax             ; guardo longitud de hash en rcx
+    inc rcx                  ; +1 para el '\0'
 
-    mov rax, [rbp-16]
-    mov rax, [rax]
-    mov rax, [rax]
-    mov [rbp-16], rax
-
-    mov rax, [rbp-8]
-    cmp rax, [rbp-16]
-    jne .loop_check
-
-    mov rax, 0
-    jmp .done
-
-.loop_start:
-    cmp qword [rbp-16], 0
-    je .init_result
-
-    mov rax, [rbp-16]
-    mov rax, [rax]
-    test rax, rax
-    jne .loop_check
-
-.init_result:
-    mov rax, [rbp-88]
-    mov rdi, rax
-    call strlen
-    mov [rbp-48], rax
-
-    mov rax, [rbp-48]
-    add rax, 1
-    mov rdi, rax
+    mov rdi, rcx             ; malloc(strlen + 1)
     call malloc
-    mov [rbp-24], rax
-
-    cmp qword [rbp-24], 0
-    jne .copy_hash
-    mov rax, 0
-    jmp .done
-
-.copy_hash:
-    mov rax, [rbp-48]
-    lea rdx, [rax+1]
-    mov rcx, [rbp-88]
-    mov rsi, rcx
-    mov rdi, [rbp-24]
-    call memcpy
-
-    cmp qword [rbp-24], 0
-    jne .prepare_loop
-    mov rax, 0
-    jmp .done
-
-.prepare_loop:
-    mov rdi, [rbp-24]
-    call strlen
-    mov [rbp-32], rax
-
-    mov rax, [rbp-72]
-    mov rax, [rax]
-    mov [rbp-40], rax
-
-.loop_nodes:
-    cmp qword [rbp-40], 0
-    je .end_loop
-
-    mov rax, [rbp-40]
-    movzx eax, byte [rax+16]
-    cmp al, [rbp-76]
-    jne .next_node
-
-    mov rax, [rbp-40]
-    mov rax, [rax+24]
     test rax, rax
-    je .next_node
+    je .return_null          ; si malloc falla
 
-    mov rdi, [rbp-40]
-    mov rdi, [rdi+24]
+    mov r10, rax             ; result = malloc(...) → r10
+
+    ; copiar hash a result
+    mov rdi, r10             ; destino
+    mov rsi, rdx             ; fuente (hash)
+    mov rdx, rcx             ; cantidad de bytes (strlen + 1)
+    call memcpy              ; memcpy(result, hash, strlen + 1)
+
+
+    mov r11, [r8]            ; current = list->first, es decir, el primer nodo de la lista
+
+    ; Detección de ciclo
+    mov r12, r11             ; slow = current
+    mov r13, r11             ; fast = current
+
+.loop_cycle_check:
+    test r13, r13            ; fast == NULL?
+    je .no_cycle_detected
+    mov r13, [r13 + 8]       ; fast = fast->next->next
+    test r13, r13
+    je .no_cycle_detected
+    mov r13, [r13 + 8]
+    mov r14, [r12 + 8]       ; slow = slow->next
+    test r12, r12
+    je .no_cycle_detected
+    cmp r12, r13             ; slow == fast?
+    je .return_null          ; ciclo detectado
+
+    mov r12, r14
+    jmp .loop_cycle_check
+
+.no_cycle_detected:
+
+    ; Continuo con la concatenación de cadenas
+    mov r11, [r8]                ; current = list->first
+
+.loop:
+    test r11, r11                ; chequeo si current es NULL
+    je .done
+
+    movzx r12, byte [r11 + 16]  ; current->type
+    cmp r12b, r9b                ; comparo current->type con type buscado
+    jne .next                    ; si no coincide, salto a next
+
+    mov r13, [r11 + 24]          ; current->hash
+    test r13, r13                ; si current->hash == NULL
+    je .next                     ; si es NULL, salto a next
+
+    ; Verificar si concatenar causaría overflow
+    mov rsi, r13                 ; rsi = current->hash
     call strlen
-    mov [rbp-56], rax
+    add rax, r10                 ; rax = current_len + add_len
+    cmp rax, MAX_RESULT_LEN      ; si excede el tamaño máximo
+    ja .return_null              ; si excede, retorno NULL
 
-    mov rdx, [rbp-32]
-    mov rax, [rbp-56]
-    add rax, rdx
-    cmp rax, 1048576
-    jbe .concat_ok
+    ; concatenar
+    mov rdi, r10                 ; rdi = string actual (result)
+    mov rsi, r13                 ; rsi = nuevo string a concatenar
+    call str_concat              ; rax = nuevo result
 
-    ; si supera el tamaño permitido
-    mov rdi, [rbp-24]
+    mov rdi, r10                 ; libero el string anterior
     call free
-    mov rax, 0
-    jmp .done
 
-.concat_ok:
-    mov rdx, [rbp-40]
-    mov rdx, [rdx+24]
-    mov rsi, rdx
-    mov rdi, [rbp-24]
-    call str_concat
-    mov [rbp-64], rax
+    mov r10, rax                 ; guardo el nuevo string concatenado en r10
 
-    cmp qword [rbp-64], 0
-    jne .replace_result
-
-    mov rdi, [rbp-24]
-    call free
-    mov rax, 0
-    jmp .done
-
-.replace_result:
-    mov rdi, [rbp-24]
-    call free
-    mov rax, [rbp-64]
-    mov [rbp-24], rax
-
-    mov rax, [rbp-56]
-    add [rbp-32], rax
-
-.next_node:
-    mov rax, [rbp-40]
-    mov rax, [rax]
-    mov [rbp-40], rax
-    jmp .loop_nodes
-
-.end_loop:
-    mov rax, [rbp-24]
+.next:
+    mov r11, [r11]               ; current = current->next, avanzo de nodo
+    jmp .loop
 
 .done:
-    mov rsp, rbp
+    mov rax, r10
+    pop rbp
+    ret
+
+.return_null:
+    xor rax, rax
     pop rbp
     ret
