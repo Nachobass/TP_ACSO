4547fd7f4287a1e3e7d989c345a2d85df3d0b76b
Ignacio <128334221+Nachobass@users.noreply.github.com>
2025-04-17 08:29:06 -0300

    mod asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -151,138 +151,162 @@ string_proc_list_add_node_asm:
 
 string_proc_list_concat_asm:
     ; rdi = list, rsi = type, rdx = hash
-    push    rbp
-    mov     rbp, rsp
-    sub     rsp, 96
-    mov     QWORD PTR [rbp-72], rdi
-    mov     eax, esi
-    mov     QWORD PTR [rbp-88], rdx
-    mov     BYTE PTR [rbp-76], al
-    cmp     QWORD PTR [rbp-72], 0
-    je      .L19
-    cmp     QWORD PTR [rbp-88], 0
-    jne     .L20
-.L19:
-    mov     eax, 0
-    jmp     .L21
-.L20:
-    mov     rax, QWORD PTR [rbp-72]
-    mov     rax, QWORD PTR [rax]
-    mov     QWORD PTR [rbp-8], rax
-    mov     rax, QWORD PTR [rbp-72]
-    mov     rax, QWORD PTR [rax]
-    mov     QWORD PTR [rbp-16], rax
-    jmp     .L22
-.L24:
-    mov     rax, QWORD PTR [rbp-8]
-    mov     rax, QWORD PTR [rax]
-    mov     QWORD PTR [rbp-8], rax
-    mov     rax, QWORD PTR [rbp-16]
-    mov     rax, QWORD PTR [rax]
-    mov     rax, QWORD PTR [rax]
-    mov     QWORD PTR [rbp-16], rax
-    mov     rax, QWORD PTR [rbp-8]
-    cmp     rax, QWORD PTR [rbp-16]
-    jne     .L22
-    mov     eax, 0
-    jmp     .L21
-.L22:
-    cmp     QWORD PTR [rbp-16], 0
-    je      .L23
-    mov     rax, QWORD PTR [rbp-16]
-    mov     rax, QWORD PTR [rax]
-    test    rax, rax
-    jne     .L24
-.L23:
-    mov     rax, QWORD PTR [rbp-88]
-    mov     rdi, rax
-    call    strlen
-    mov     QWORD PTR [rbp-48], rax
-    mov     rax, QWORD PTR [rbp-48]
-    add     rax, 1
-    mov     rdi, rax
-    call    malloc
-    mov     QWORD PTR [rbp-24], rax
-    cmp     QWORD PTR [rbp-24], 0
-    jne     .L25
-    mov     eax, 0
-    jmp     .L21
-.L25:
-    mov     rax, QWORD PTR [rbp-48]
-    lea     rdx, [rax+1]
-    mov     rcx, QWORD PTR [rbp-88]
-    mov     rax, QWORD PTR [rbp-24]
-    mov     rsi, rcx
-    mov     rdi, rax
-    call    memcpy
-    cmp     QWORD PTR [rbp-24], 0
-    jne     .L26
-    mov     eax, 0
-    jmp     .L21
-.L26:
-    mov     rax, QWORD PTR [rbp-24]
-    mov     rdi, rax
-    call    strlen
-    mov     QWORD PTR [rbp-32], rax
-    mov     rax, QWORD PTR [rbp-72]
-    mov     rax, QWORD PTR [rax]
-    mov     QWORD PTR [rbp-40], rax
-    jmp     .L27
-.L31:
-    mov     rax, QWORD PTR [rbp-40]
-    movzx   eax, BYTE PTR [rax+16]
-    cmp     BYTE PTR [rbp-76], al
-    jne     .L28
-    mov     rax, QWORD PTR [rbp-40]
-    mov     rax, QWORD PTR [rax+24]
-    test    rax, rax
-    je      .L28
-    mov     rax, QWORD PTR [rbp-40]
-    mov     rax, QWORD PTR [rax+24]
-    mov     rdi, rax
-    call    strlen
-    mov     QWORD PTR [rbp-56], rax
-    mov     rdx, QWORD PTR [rbp-32]
-    mov     rax, QWORD PTR [rbp-56]
-    add     rax, rdx
-    cmp     rax, 1048576
-    jbe     .L29
-    mov     rax, QWORD PTR [rbp-24]
-    mov     rdi, rax
-    call    free
-    mov     eax, 0
-    jmp     .L21
-.L29:
-    mov     rax, QWORD PTR [rbp-40]
-    mov     rdx, QWORD PTR [rax+24]
-    mov     rax, QWORD PTR [rbp-24]
-    mov     rsi, rdx
-    mov     rdi, rax
-    call    str_concat
-    mov     QWORD PTR [rbp-64], rax
-    cmp     QWORD PTR [rbp-64], 0
-    jne     .L30
-    mov     rax, QWORD PTR [rbp-24]
-    mov     rdi, rax
-    call    free
-    mov     eax, 0
-    jmp     .L21
-.L30:
-    mov     rax, QWORD PTR [rbp-24]
-    mov     rdi, rax
-    call    free
-    mov     rax, QWORD PTR [rbp-64]
-    mov     QWORD PTR [rbp-24], rax
-    mov     rax, QWORD PTR [rbp-56]
-    add     QWORD PTR [rbp-32], rax
-.L28:
-    mov     rax, QWORD PTR [rbp-40]
-    mov     rax, QWORD PTR [rax]
-    mov     QWORD PTR [rbp-40], rax
-.L27:
-    cmp     QWORD PTR [rbp-40], 0
-    jne     .L31
-    mov     rax, QWORD PTR [rbp-24]
-.L21:
-    leave
+    push rbp
+    mov rbp, rsp
+    sub rsp, 96            ; reservar espacio local
+
+    mov [rbp-72], rdi      ; list
+    movzx eax, sil         ; pasar type como 8 bits → eax
+    mov [rbp-88], rdx      ; hash
+    mov [rbp-76], al       ; guardar type en [rbp-76]
+
+    cmp qword [rbp-72], 0
+    je .return_null
+    cmp qword [rbp-88], 0
+    jne .continue
+.return_null:
+    mov rax, 0
+    jmp .done
+
+.continue:
+    mov rax, [rbp-72]
+    mov rax, [rax]
+    mov [rbp-8], rax       ; aux1
+    mov rax, [rbp-72]
+    mov rax, [rax]
+    mov [rbp-16], rax      ; current
+
+.loop_check:
+    mov rax, [rbp-8]
+    mov rax, [rax]
+    mov [rbp-8], rax
+
+    mov rax, [rbp-16]
+    mov rax, [rax]
+    mov rax, [rax]
+    mov [rbp-16], rax
+
+    mov rax, [rbp-8]
+    cmp rax, [rbp-16]
+    jne .loop_check
+
+    mov rax, 0
+    jmp .done
+
+.loop_start:
+    cmp qword [rbp-16], 0
+    je .init_result
+
+    mov rax, [rbp-16]
+    mov rax, [rax]
+    test rax, rax
+    jne .loop_check
+
+.init_result:
+    mov rax, [rbp-88]
+    mov rdi, rax
+    call strlen
+    mov [rbp-48], rax
+
+    mov rax, [rbp-48]
+    add rax, 1
+    mov rdi, rax
+    call malloc
+    mov [rbp-24], rax
+
+    cmp qword [rbp-24], 0
+    jne .copy_hash
+    mov rax, 0
+    jmp .done
+
+.copy_hash:
+    mov rax, [rbp-48]
+    lea rdx, [rax+1]
+    mov rcx, [rbp-88]
+    mov rsi, rcx
+    mov rdi, [rbp-24]
+    call memcpy
+
+    cmp qword [rbp-24], 0
+    jne .prepare_loop
+    mov rax, 0
+    jmp .done
+
+.prepare_loop:
+    mov rdi, [rbp-24]
+    call strlen
+    mov [rbp-32], rax
+
+    mov rax, [rbp-72]
+    mov rax, [rax]
+    mov [rbp-40], rax
+
+.loop_nodes:
+    cmp qword [rbp-40], 0
+    je .end_loop
+
+    mov rax, [rbp-40]
+    movzx eax, byte [rax+16]
+    cmp al, [rbp-76]
+    jne .next_node
+
+    mov rax, [rbp-40]
+    mov rax, [rax+24]
+    test rax, rax
+    je .next_node
+
+    mov rdi, [rbp-40]
+    mov rdi, [rdi+24]
+    call strlen
+    mov [rbp-56], rax
+
+    mov rdx, [rbp-32]
+    mov rax, [rbp-56]
+    add rax, rdx
+    cmp rax, 1048576
+    jbe .concat_ok
+
+    ; si supera el tamaño permitido
+    mov rdi, [rbp-24]
+    call free
+    mov rax, 0
+    jmp .done
+
+.concat_ok:
+    mov rdx, [rbp-40]
+    mov rdx, [rdx+24]
+    mov rsi, rdx
+    mov rdi, [rbp-24]
+    call str_concat
+    mov [rbp-64], rax
+
+    cmp qword [rbp-64], 0
+    jne .replace_result
+
+    mov rdi, [rbp-24]
+    call free
+    mov rax, 0
+    jmp .done
+
+.replace_result:
+    mov rdi, [rbp-24]
+    call free
+    mov rax, [rbp-64]
+    mov [rbp-24], rax
+
+    mov rax, [rbp-56]
+    add [rbp-32], rax
+
+.next_node:
+    mov rax, [rbp-40]
+    mov rax, [rax]
+    mov [rbp-40], rax
+    jmp .loop_nodes
+
+.end_loop:
+    mov rax, [rbp-24]
+
+.done:
+    mov rsp, rbp
+    pop rbp
     ret
