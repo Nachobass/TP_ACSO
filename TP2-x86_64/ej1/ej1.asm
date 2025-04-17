451d20365a127b9db8ea7ed2d2e178f6850d2073
Ignacio <128334221+Nachobass@users.noreply.github.com>
2025-04-17 09:06:23 -0300

    mod

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -68,11 +68,9 @@ string_proc_node_create_asm:
     mov qword [rax], 0      ; next
     mov qword [rax + 8], 0      ; previous
     mov byte  [rax + 16], cl    ; type
-    mov qword [rbx + 24], rdx   ; hash
+    mov qword [rax + 24], rdx   ; hash
 
-    mov rax, rbx                ; devolver el puntero al nodo
-
-    pop rbx
+    mov rsp, rbp
     pop rbp
     ret
 
